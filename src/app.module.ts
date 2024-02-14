d6303af951b6911159bd8c48969087f6a8415828
NikkyXO <alexisdame2017@gmail.com>
2024-02-14 13:01:45 +0100

    add webhook url

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StripeModule } from './modules/stripe/stripe.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UserModule],
+  imports: [AuthModule, DatabaseModule, UserModule, StripeModule],
   controllers: [AppController],
   providers: [AppService],
 })
